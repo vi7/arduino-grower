859dc1343e27cbc9f56d89f8a5223029bf323568
Vitaliy Dmitriev <vi7alya@gmail.com>
2019-04-21 22:12:57 +0200

    Emergency power-off implemented

--- temp-rh-pwr-control/temp-rh-pwr-control.ino
+++ temp-rh-pwr-control/temp-rh-pwr-control.ino
@@ -1,14 +1,18 @@
-/* taken from https://escapequotes.net/esp8266-dht22-temperature-sensor/ */
-
-// Comment this out to disable prints and save space:
-//#define BLYNK_PRINT Serial
+/* temp-rh-pwr-control.ino */
 
 #include <ESP8266WiFi.h>
 //#include <BlynkSimpleEsp8266.h>
 #include <SimpleTimer.h>
 #include <DHTesp.h>
 
+// Comment this out to disable Blynk prints and save space:
+//#define BLYNK_PRINT Serial
 #define DHTPIN D0
+#define RELAYPIN D1
+// relay NC output is closed
+#define RELAY_ON LOW
+// relay NC output is opened
+#define RELAY_OFF HIGH
 
 ////////////////////
 // Blynk Settings //
@@ -17,39 +21,56 @@
 //char WiFiNetwork[] = "xxxxxxWlan";
 //char WiFiPassword[] = "xxx_xxx_xxx_xxx";
 
+const uint8_t MAX_TEMP = 40;
+const uint8_t TEMP_HYSTERESIS = 5;
+
 SimpleTimer timer;
 DHTesp dht;
 float temp,rH;
 long dhtReadInterval;
 
+/*********/ 
+/* SETUP */
+/*********/
 void setup() {
-    Serial.begin(115200);
-    String thisBoard = ARDUINO_BOARD;
-    Serial.print("\n\n"+thisBoard);
-    Serial.println(F(" is up. Hey there!"));
-
-    dht.setup(DHTPIN, DHTesp::DHT22);
-    dhtReadInterval = (dht.getMinimumSamplingPeriod());
-    timer.setInterval(dhtReadInterval, sendData);
+  
+  // init Serial for logging and say Hi
+  Serial.begin(115200);
+  const String boardIdentity = ARDUINO_BOARD;
+  Serial.print("\n\n" + boardIdentity);
+  Serial.println(F(" is up. Hey there!"));
+  
+  initDHT();
+  initRelay();
+  
+  // schedule dataHandler() func execution
+  timer.setInterval(dhtReadInterval, dataHandler);
 
 //    // Initialize Blynk
 //    Blynk.begin(BlynkAuth, WiFiNetwork, WiFiPassword);
 
 }
 
+/********/ 
+/* LOOP */
+/********/
 void loop() {
   
 //    Blynk.run();
 
-    timer.run();
+  timer.run();
 }
 
-void sendData()
-{
+/*************/ 
+/* FUNCTIONS */
+/*************/ 
+void dataHandler() {
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
   
   getTempRh();
+  
+  // TODO: implement the ability to swith logging on and off
   Serial.print(dht.getStatusString());
   Serial.print(F("\t"));
   Serial.print(F("temperature: "));
@@ -57,17 +78,42 @@ void sendData()
   Serial.print(F("\t\t"));
   Serial.print(F("RH: "));
   Serial.println(rH);
+  
+  checkTemp();
 
 //  Blynk.virtualWrite(10, t); // virtual pin
 //  Blynk.virtualWrite(11, h); // virtual pin
 
 }
 
+void initDHT() {
+  dht.setup(DHTPIN, DHTesp::DHT22);
+  dhtReadInterval = (dht.getMinimumSamplingPeriod());
+}
+
+void initRelay() {
+  pinMode(RELAYPIN, OUTPUT);
+  digitalWrite(RELAYPIN, RELAY_ON);
+}
+
 void getTempRh() {
   rH = dht.getHumidity();
   temp = dht.getTemperature();
   if (isnan(rH) || isnan(temp)) {
-      Serial.println(F("Failed to read from DHT sensor!"));
-      return;
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
   }
+}
+
+void checkTemp() {
+  if (temp >= MAX_TEMP) {
+    powerOff(true);
+  }
+  else if (temp < MAX_TEMP - TEMP_HYSTERESIS) {
+    powerOff(false);
+  }
+}
+
+void powerOff(bool isOff) {
+  isOff ? digitalWrite(RELAYPIN, RELAY_OFF) : digitalWrite(RELAYPIN, RELAY_ON);
 }
