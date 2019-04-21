d399468cda812f63e3439e986198c9e4cee34663
Vitaliy Dmitriev <vi7alya@gmail.com>
2019-04-21 13:48:16 +0200

    Temperature and RH monitoring implemented

--- README.md
+++ README.md
@@ -11,13 +11,18 @@ Temperature, RH and power control
 List of Arduino IDE libs
 
 - [ESP8266 board support](https://github.com/esp8266/Arduino#installing-with-boards-manager)
-- Adafruit unified sensor lib by Adafruit
-- DHT sensor lib by Adafruit. Tutorial: https://learn.adafruit.com/dht
+- [DHT sensor library for ESPx](https://github.com/beegee-tokyo/DHTesp)
 - [Blynk lib](https://github.com/blynkkk/blynk-library/releases) for integration with [Blynk IoT](https://blynk.io)
+- [SimpleTimer lib](https://github.com/schinken/SimpleTimer)
 
 **Hardware**
 
 - [Wemos D1 mini](https://wiki.wemos.cc/products:d1:d1_mini) or compatible clone
-- [DHT22 (AM2302) sensor](https://learn.adafruit.com/dht)
+- [DHT22 (AM2302) sensor](https://learn.adafruit.com/dht). See notes below
 - 230V relay switch
 
+# Setup
+
+DHT22 used in the project has been already shipped with PCB and pullup resistor. [Click](resources/dht22-am2302-temperature-humidity.jpeg) to see the picture. Thus should be connected to the `D0` pin of the D1 mini board (no pullup)
+
+Schematics TBD
--- temp-rh-pwr-control/temp-rh-pwr-control.ino
+++ temp-rh-pwr-control/temp-rh-pwr-control.ino
@@ -1,101 +1,73 @@
-// taken from https://escapequotes.net/esp8266-dht22-temperature-sensor/
+/* taken from https://escapequotes.net/esp8266-dht22-temperature-sensor/ */
 
-// TODO: refactor that
+// Comment this out to disable prints and save space:
+//#define BLYNK_PRINT Serial
 
-#define BLYNK_PRINT Serial // Comment this out to disable prints and save space
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+//#include <BlynkSimpleEsp8266.h>
 #include <SimpleTimer.h>
-#include <DHT.h>
+#include <DHTesp.h>
 
-#define DHTPIN D4 //pin gpio 2 in sensor
-
-DHT dht(DHTPIN, DHT22);
-//static const uint8_t SDA = 4;
-//static const uint8_t SCL = 5;
-//
-//static const uint8_t LED_BUILTIN = 2;
-//static const uint8_t BUILTIN_LED = 2;
-//
-//static const uint8_t D0 = 16;
-//static const uint8_t D1 = 5;
-//static const uint8_t D2 = 4;
-//static const uint8_t D3 = 0;
-//static const uint8_t D4 = 2;
-//static const uint8_t D5 = 14;
-//static const uint8_t D6 = 12;
-//static const uint8_t D7 = 13;
-//static const uint8_t D8 = 15;
-//static const uint8_t RX = 3;
-//static const uint8_t TX = 1;
+#define DHTPIN D0
 
 ////////////////////
 // Blynk Settings //
 ////////////////////
-char BlynkAuth[] = "142da76c91af47b192d396xxxxxxxxx";
-char WiFiNetwork[] = "xxxxxxWlan";
-char WiFiPassword[] = "xxx_xxx_xxx_xxx";
-SimpleTimer timer;
+//char BlynkAuth[] = "142da76c91af47b192d396xxxxxxxxx";
+//char WiFiNetwork[] = "xxxxxxWlan";
+//char WiFiPassword[] = "xxx_xxx_xxx_xxx";
 
-float t,h;
-unsigned long previousMillis = 0; // will store last temp was read
-const long interval = 2000; // interval at which to read sensor
+SimpleTimer timer;
+DHTesp dht;
+float temp,rH;
+long dhtReadInterval;
 
 void setup() {
-    Serial.begin(115200); // Serial
-    // put your setup code here, to run once:
-    pinMode(BUILTIN_LED, OUTPUT); // initialize onboard LED as output
-    dht.begin();
-    // Initialize Blynk
-    Blynk.begin(BlynkAuth, WiFiNetwork, WiFiPassword);
-    // Setup a function to be called 30 second
-    timer.setInterval(30000L, sendUptime);
+    Serial.begin(115200);
+    String thisBoard = ARDUINO_BOARD;
+    Serial.print("\n\n"+thisBoard);
+    Serial.println(F(" is up. Hey there!"));
+
+    dht.setup(DHTPIN, DHTesp::DHT22);
+    dhtReadInterval = (dht.getMinimumSamplingPeriod());
+    timer.setInterval(dhtReadInterval, sendData);
+
+//    // Initialize Blynk
+//    Blynk.begin(BlynkAuth, WiFiNetwork, WiFiPassword);
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    digitalWrite(BUILTIN_LED, HIGH); // turn on LED with voltage HIGH
-    delay(1000); // wait one second
-    digitalWrite(BUILTIN_LED, LOW); // turn off LED with voltage LOW
-    delay(1000); // wait one second
-    Blynk.run();
+  
+//    Blynk.run();
+
     timer.run();
 }
 
-void sendUptime()
+void sendData()
 {
-    // You can send any value at any time.
-    // Please don't send more that 10 values per second.
-    //Read the Temp and Humidity from DHT
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  
+  getTempRh();
+  Serial.print(dht.getStatusString());
+  Serial.print(F("\t"));
+  Serial.print(F("temperature: "));
+  Serial.print(temp);
+  Serial.print(F("\t\t"));
+  Serial.print(F("RH: "));
+  Serial.println(rH);
 
-    gettemperature();
-    Serial.print("temp:");
-    Serial.println(t);
-    Serial.print("humi:");
-    Serial.println(h);
-    Blynk.virtualWrite(10, t); // virtual pin
-    Blynk.virtualWrite(11, h); // virtual pin
+//  Blynk.virtualWrite(10, t); // virtual pin
+//  Blynk.virtualWrite(11, h); // virtual pin
+
 }
 
-void gettemperature() {
-    // Wait at least 2 seconds seconds between measurements.
-    // if the difference between the current time and last time you read
-    // the sensor is bigger than the interval you set, read the sensor
-    // Works better than delay for things happening elsewhere also
-    unsigned long currentMillis = millis();
-
-    if(currentMillis - previousMillis >= interval) {
-        // save the last time you read the sensor
-        previousMillis = currentMillis;
-
-        // Reading temperature for humidity takes about 250 milliseconds!
-        // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-        h = dht.readHumidity(); // Read humidity (percent)
-        t = dht.readTemperature(); // Read temperature as C
-        // Check if any reads failed and exit early (to try again).
-        if (isnan(h) || isnan(t)) {
-            Serial.println("Failed to read from DHT sensor!");
-            return;
-        }
-    }
+void getTempRh() {
+  rH = dht.getHumidity();
+  temp = dht.getTemperature();
+  if (isnan(rH) || isnan(temp)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+  }
 }
